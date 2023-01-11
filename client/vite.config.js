d6ac31b93dbffea57d19ac3be0c1ea23cee02b66
Keiber Urbila <keiberup.dev@gmail.com>
2023-01-10 22:06:52 -0400

    feat: vite preview port change to 8000

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, preview } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 8000
+  },
+  preview: {
     port: 8000
   }
 })
